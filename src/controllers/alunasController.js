4b176c2dee0c74bf1be3799261dc2651e84ce325
KateLucena <kate.lucena2011@gmail.com>
2019-10-09 22:18:20 -0300

    Construindo API

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -7,6 +7,29 @@ exports.get = (req, res) => {
 
 exports.getById = (req, res) => {
     const id = req.params.id
+
+    if( id == 0 || id > 17){
+        res.redirect(301, "https://www.mercadolivre.com.br")
+    }
     console.log(id)
     res.status(200).send(alunas.find(aluna => aluna.id == id))
 }
+exports.getBooks = (req, res) => {
+    const id = req.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+
+    if(!aluna){
+        res.send("Nao encontrei essa aluna")
+    }
+    const livrosAluna = aluna.livros
+    const tituloLivros = livrosAluna.map(livro => livro.titulo)
+    res.status(200).send(tituloLivros)
+}
+exports.getSp = (req, res) => {
+    
+    const sp = alunas.filter(aluna => aluna.nasceuEmSp == 'true')
+    
+    const paulista = sp.map(aluna => aluna.nome)
+    res.status(200).send(paulista)
+}
+
